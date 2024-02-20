34da0ec51b361ee9a165a8be7157895ca4bfa059
Mamatha Kothakota <kothakotamamatha2001@gmail.com>
2024-02-20 14:22:58 +0530

    added backend code

--- billing-reports/src/main/java/com/billingreports/entities/user/User.java
+++ billing-reports/src/main/java/com/billingreports/entities/user/User.java
@@ -1,15 +1,12 @@
 package com.billingreports.entities.user;
 
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,32 +14,21 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String id;
-
-    @NotNull
-    @NotBlank
+    @NotEmpty(message = "Firstname should not be empty")
     private String firstName;
-
-    @NotNull
-    @NotBlank
+    @NotEmpty(message = "Lastname should not be empty")
     private String lastName;
-
-    @NotNull
-    @NotBlank
+    @NotEmpty(message = "Username should not be empty")
     @Indexed(unique = true)
     private String userName;
-
-    @NotNull
-    @NotBlank
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Please enter a valid email address")
     @Indexed(unique = true)
     private String email;
-
-    @NotNull
-    @NotBlank
+    @NotEmpty(message = "Password should not be empty")
+    @Size(min = 6, message = "Password should contain at least 6 characters")
     private String password;
-
-    @NotNull
-    @NotBlank
     private String roles;
-
     private boolean isActive;
 }
+ 
--- billing-reports/src/main/java/com/billingreports/exceptions/GlobalExceptionHandler.java
+++ billing-reports/src/main/java/com/billingreports/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
 package com.billingreports.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UsernameOrEmailAlreadyExistsException.class)
     public ResponseEntity<ErrorDetails> userNameAlreadyExistException(UsernameOrEmailAlreadyExistsException exception, WebRequest webRequest) {
@@ -44,5 +54,19 @@ public class GlobalExceptionHandler {
         LocalDateTime localDate = LocalDateTime.now();
         ErrorDetails errorDetails = new ErrorDetails(localDate, exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        List<ObjectError> errorList = ex.getBindingResult().getAllErrors();
+
+        errorList.forEach((error) ->{
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
